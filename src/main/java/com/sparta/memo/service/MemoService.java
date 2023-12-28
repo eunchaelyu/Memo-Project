77897bd850aa2e3145fd652bcd97df116bb9b883
eunchaelyu <hn04133@naver.com>
2023-12-28 14:38:54 +0900

    IoC Container Bean

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,16 +4,18 @@ import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
+
+import org.springframework.stereotype.Service;
 
 
 import java.util.List;
-
+@Service
 public class MemoService {
 
     private final MemoRepository memoRepository;
 
-    public MemoService(MemoRepository memoRepository) {
+
+    public MemoService(MemoRepository memoRepository) { // 생성자 주입
         this.memoRepository= memoRepository;
     }
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
